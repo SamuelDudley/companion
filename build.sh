9bb2625e9397c74518af7e6d1cb2700f1dcac6b1
samuel dudley <dudley.samuel@gmail.com>
2017-07-10 18:26:53 +0930

    update build script and .gitignore

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 echo "Configuring and building companion project..."
 
+echo "Updating submodules..."
+git submodule update --init --recursive
+
+echo "Generating mavlink headders..."
 python ./modules/mavlink/pymavlink/tools/mavgen.py --lang C++11 ./modules/mavlink/message_definitions/v1.0/ardupilotmega.xml -o ./generated/mavlink --wire-protocol=2.0
 
+echo "Doing the build..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
